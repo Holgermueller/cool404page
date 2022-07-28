325580798aac6d5ccc8cdcc45cc67c640aa409a3
Holger Mueller <9jholger@gmail.com>
2022-07-27 20:42:20 -0500

    got content from contentful onto dom

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   siteName: "Holger Mueller",
-  plugins: [],
+  plugins: [
+    {
+      use: "@gridsome/source-contentful",
+      options: {
+        space: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+        host: "cdn.contentful.com",
+        environment: "master",
+        typeName: "Contentful",
+      },
+    },
+  ],
 };
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,14 +2,57 @@
   <Layout>
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
 
-    <h1>Hello, world!</h1>
+    <p>
+      {{ $page.allContentfulBlogpost.edges[0].node.title }}
+    </p>
+
+    <p>
+      {{ $page.allContentfulBlogpost.edges[0].node.location }}
+    </p>
+
+    <p>
+      {{ $page.allContentfulBlogpost.edges[0].node.listeningTo }}
+    </p>
+
+    <hr />
+
+    <p>
+      {{ richtextToHtml($page.allContentfulBlogpost.edges[0].node.blogBody) }}
+    </p>
   </Layout>
 </template>
 
+<page-query>
+query Blank {
+  allContentfulBlogpost {
+    edges {
+      node {
+        title
+        location
+        listeningTo
+        blogBody
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import { Pager } from "gridsome";
+
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
 export default {
   metaInfo: {
     title: "Hello, world!",
+  },
+  components: {
+    Pager,
+  },
+  methods: {
+    richtextToHtml(content) {
+      return documentToHtmlString(content);
+    },
   },
 };
 </script>
